1f7e06febc134a9ee393714bf0ace89650a9f96f
Banin Abrar <baninabrar98@gmail.com>
2019-11-26 18:55:39 +0600

    Added high and low to forecast

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,8 +1,8 @@
 const request = require('request')
 
 const forecast = (latitude, longitude, callback) => {
-    const uri = "https://api.darksky.net/forecast/52bf2b749f1b4263fd343024c176c021/" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "?units=si"
-    request({ uri, json: true }, (error, {body}) => {
+    const uri = "https://api.darksky.net/forecast/52bf2b749f1b4263fd343024c176c021/" + encodeURIComponent(latitude) + "," + encodeURIComponent(longitude) + "?units=si&exclude=minutely,flags,alerts"
+    request({ uri, json: true }, (error, { body }) => {
         if (error) {
             callback("Unable to connect to weather service.", undefined)
         }
@@ -11,7 +11,10 @@ const forecast = (latitude, longitude, callback) => {
             callback("Unable to find weather", undefined)
         }
         else {
-            const msg = body.hourly.summary + ". It is currently " + body.currently.temperature + " degrees. There is a " + body.currently.precipProbability + "% chance of rain."
+            const msg = body.hourly.summary + " Expected high of " + body.daily.data[0].temperatureHigh + " °C" +
+                " and a low of " + body.daily.data[0].temperatureLow + " °C" +
+                ". It is currently " + body.currently.temperature + " °C" +
+                ". There is a " + body.currently.precipProbability + "% chance of rain."
             callback(undefined, msg)
         }
     })
